555fbafcd4210b5209763d79a3d9dd8ace6e3535
Adones Balica <adonesbalica@gmail.com>
2026-01-22 09:18:43 -0300

    feat: send activation email after `user` registration

---
+++ models/activation.js
@@ -0,0 +1,71 @@
+import database from "infra/database";
+import email from "infra/email";
+import webserver from "infra/webserver";
+
+const EXPIRATION_IN_MILLISECONDS = 60 * 15 * 1000; // 15 minutes
+
+async function create(userId) {
+  const expiresAt = new Date(Date.now() + EXPIRATION_IN_MILLISECONDS);
+
+  const newToken = await runInsertQuery(userId, expiresAt);
+  return newToken;
+
+  async function runInsertQuery(userId, expiresAt) {
+    const results = await database.query({
+      text: `
+        INSERT INTO
+          user_activation_tokens (user_id, expires_at)
+        VALUES
+          ($1, $2)
+        RETURNING
+          *
+      ;`,
+      values: [userId, expiresAt],
+    });
+
+    return results.rows[0];
+  }
+}
+
+async function findOneByUserId(userId) {
+  const user = await runSelectQuery(userId);
+  return user;
+
+  async function runSelectQuery(userId) {
+    const results = await database.query({
+      text: `
+        SELECT
+          *
+        FROM
+          user_activation_tokens
+        WHERE
+          user_id = $1
+      ;`,
+      values: [userId],
+    });
+
+    return results.rows[0];
+  }
+}
+
+async function sendEmailToUser(user, activationToken) {
+  await email.send({
+    from: "FinTab <contato@fintab.com.br>",
+    to: user.email,
+    subject: "Ative seu cadastro no FinTab!",
+    text: `${user.username}, clique no link a baixo para ativar o seu cadastro no FinTab:
+
+${webserver.origin}/cadastro/ativar/${activationToken.id}
+    
+Atenciosamente,
+Equipe FinTab`,
+  });
+}
+
+const activation = {
+  create,
+  findOneByUserId,
+  sendEmailToUser,
+};
+
+export default activation;
